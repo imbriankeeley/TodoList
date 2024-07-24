e65a6ae462e8364b199f7dbac2e2e1d08c87fa8e
Brian Keeley <imbriankeeley@gmail.com>
2024-07-24 15:37:16 -0400

    Connected css to index file

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+import './styles.css';
+
 library.add(fas, far, fab) 
 
 dom.i2svg() 
